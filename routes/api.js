dc468cb20c52c4ec3055f936126f8d8cd7eb9a4a
cloozo <mybenefitchoiceinc@gmail.com>
2022-02-03 06:30:02 -0500

    asset folder adedd

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
 
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
@@ -20,7 +11,15 @@ router.post("/api/transaction/bulk", ({body}, res) => {
       res.status(404).json(err);
     });
 });
-
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
